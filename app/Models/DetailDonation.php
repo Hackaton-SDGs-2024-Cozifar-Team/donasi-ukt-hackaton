9bff4e545acd3c13f240ddf0a3160c69b6ff7fe1
farhanariyanto03 <ariyantofarhan03@gmail.com>
2024-06-09 15:20:28 +0700

    update model
    penamaan yang sebelumnya salah

--- app/Models/DetailDonation.php
+++ app/Models/DetailDonation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class donation_registration extends Model
+class DetailDonation extends Model
 {
     use HasFactory;
 }
