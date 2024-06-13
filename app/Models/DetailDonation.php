fa1e13f1d3520e2363e7ef988cee917d46e8c18f
farhanariyanto03 <ariyantofarhan03@gmail.com>
2024-06-13 23:28:18 +0700

    foreign key
    di detail donasi

--- app/Models/DetailDonation.php
+++ app/Models/DetailDonation.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DetailDonation extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_detail_donation';
+
+    public function donation() : HasOne
+    {
+        return $this->HasOne(Donation::class, 'id_donation','id_donation');
+    }
+
+    public function users() : HasOne
+    {
+        return $this->hasOne(User::class, "id", "id_user");
+    }
 }
--- app/Models/donation.php
+++ app/Models/donation.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Donation extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id_donation';
+
+    
 }
