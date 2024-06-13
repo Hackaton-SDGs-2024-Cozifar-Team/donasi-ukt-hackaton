616c755a299458c9259ca5c20d9bb4b011766945
farhanariyanto03 <ariyantofarhan03@gmail.com>
2024-06-14 01:02:25 +0700

    Donatur
    Muncul data donatur

--- app/Models/DetailDonation.php
+++ app/Models/DetailDonation.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DetailDonation extends Model
 {
     use HasFactory;
-    
+    protected $primaryKey = 'id_detail_donation';
+
+    public function donation() : BelongsTo
+    {
+        return $this->belongsTo(Donation::class, 'id_donation', 'id_donation');
+    }
+
+    public function periode() : BelongsTo
+    {
+        return $this->belongsTo(Periode::class, 'id_periode', 'id_periode');
+    }
 }
--- app/Models/donation.php
+++ app/Models/donation.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Donation extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id_donation';
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id');
+    }
 }
